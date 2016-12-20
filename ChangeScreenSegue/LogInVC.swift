d89276f5ba444f8871f75a6e42c41091e6b6f779
Erik Uecke <erik.uecke@digitalfilmacademy.edu>
2016-12-20 15:09:56 -0500

    Segues segued

---
+++ ChangeScreenSegue/LogInVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChangeScreenSegue
+//
+//  Created by Erik Uecke on 12/20/16.
+//  Copyright © 2016 Erik Uecke. All rights reserved.
+//
+
+import UIKit
+
+class LogInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+    }
+
+    
+}
+
---
+++ ChangeScreenSegue/MusicListVC.swift
@@ -0,0 +1,47 @@
+//
+//  MusicListVC.swift
+//  ChangeScreenSegue
+//
+//  Created by Erik Uecke on 12/20/16.
+//  Copyright © 2016 Erik Uecke. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.blue
+    }
+
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func load3rdScreenPressed(_ sender: Any) {
+        performSegue(withIdentifier: "PlaySongVC", sender: "Quit Playing Games")
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? PlaySongVC {
+            
+            if let song = sender as? String {
+                destination.selectedSong = song
+            }
+            
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
